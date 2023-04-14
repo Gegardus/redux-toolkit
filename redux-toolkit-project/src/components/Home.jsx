f215d28a80b2e7ae8929a6ec00e312ed68c93499
Gegardus <stgegard@gmail.com>
2023-04-14 12:17:12 +0400

    Working with APIs in Redux Toolkit

--- redux-toolkit-project/src/components/Home.jsx
+++ redux-toolkit-project/src/components/Home.jsx
@@ -1,19 +1,24 @@
+// ===========Fetching API============
 import { useDispatch, useSelector } from 'react-redux'
-import productList from '../data/productList.json'
 import cartSlice from '../data/cartSlice'
+import { fetchAllProducts } from '../data/productSlice'
 import '../styles/home.scss'
+import { useEffect } from 'react'
 
 const Home = () => {
-  const { cartProductIds } = useSelector((state) => state.cart)
+  const state = useSelector((state) => state)
+  const { cart, products } = state
   const { addToCart, removeFromCart } = cartSlice.actions
   const dispatch = useDispatch()
 
-  console.log(cartProductIds);
+  useEffect(() => {
+    dispatch(fetchAllProducts('http://localhost:3000/products'))
+  }, [dispatch])
 
   return (
     <div className="container product-catalogue">
       <div className="row">
-        {productList.products.map((product) => {
+        {products.data?.map((product) => {
           return (
             <div className="wrapper col-md-4" key={product.id}>
               <div className="card">
@@ -23,17 +28,16 @@ const Home = () => {
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text">${product.price}</p>
 
-                  {!cartProductIds.includes(product.id) && (
+                  {!cart.cartProductIds.includes(product.id) && (
                     <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>
                       Add to cart
                     </button>
                   )}
-                  {cartProductIds.includes(product.id) && (
+                  {cart.cartProductIds.includes(product.id) && (
                     <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
                       Remove from cart
                     </button>
                   )}
-
                 </div>
               </div>
             </div>
@@ -45,3 +49,54 @@ const Home = () => {
 }
 
 export default Home
+
+
+// ===========Redux Toolkit============
+
+// import { useDispatch, useSelector } from 'react-redux'
+// import productList from '../data/productList.json'
+// import cartSlice from '../data/cartSlice'
+// import '../styles/home.scss'
+
+// const Home = () => {
+//   const { cartProductIds } = useSelector((state) => state.cart)
+//   const { addToCart, removeFromCart } = cartSlice.actions
+//   const dispatch = useDispatch()
+
+//   console.log(cartProductIds);
+
+//   return (
+//     <div className="container product-catalogue">
+//       <div className="row">
+//         {productList.products.map((product) => {
+//           return (
+//             <div className="wrapper col-md-4" key={product.id}>
+//               <div className="card">
+//                 <img className="card-img-top center-block" src={product.imageUrl} alt="Card cap" />
+
+//                 <div className="card-body text-center">
+//                   <h5 className="card-title">{product.name}</h5>
+//                   <p className="card-text">${product.price}</p>
+
+//                   {!cartProductIds.includes(product.id) && (
+//                     <button className="btn btn-primary" onClick={() => dispatch(addToCart(product.id))}>
+//                       Add to cart
+//                     </button>
+//                   )}
+//                   {cartProductIds.includes(product.id) && (
+//                     <button className="btn btn-primary" onClick={() => dispatch(removeFromCart(product.id))}>
+//                       Remove from cart
+//                     </button>
+//                   )}
+
+//                 </div>
+//               </div>
+//             </div>
+//           )
+//         })}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Home
